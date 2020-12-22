652848497b28b0f7f90fcf4f71b83c049a0b8be8
Dave Amiana <amianism25@gmail.com>
2020-12-22 22:48:17 +0800

    continuous distribution is updated.

--- Statistics/continuousDist.py
+++ Statistics/continuousDist.py
@@ -47,7 +47,7 @@ class Uniform:
     def pdf(self, plot=False, xlim=None, ylim=None, xlabel=None, ylabel=None):
         '''
         Args:
-            plot (bool): returns scatter plot if true. 
+            plot (bool): returns plot if true. 
             xlim(float): sets x axis ∈ [-xlim, xlim]. Only relevant when plot is true.
             ylim(float): sets y axis ∈[0,ylim]. Only relevant when plot is true. 
             xlabel(string): sets label in x axis. Only relevant when plot is true. 
@@ -70,7 +70,7 @@ class Uniform:
     def cdf(self, plot=False, xlim=None, ylim=None, xlabel=None, ylabel=None):
         '''
         Args:
-            plot (bool): returns scatter plot if true. 
+            plot (bool): returns plot if true. 
             xlim(float): sets x axis ∈ [-xlim, xlim]. Only relevant when plot is true.
             ylim(float): sets y axis ∈[0,ylim]. Only relevant when plot is true. 
             xlabel(string): sets label in x axis. Only relevant when plot is true. 
@@ -98,12 +98,18 @@ class Uniform:
         return generator(a, b, threshold)  # what does it really say?
 
 
-# add method for computing normal distribution given (self.mean, self.std, self.randvar)
+# add method for confidence intervals
 class Normal(Base):
     '''
     This class contains implementation of the Normal Distribution for calculating the
     probablity density function and cumulative distirbution function. Additionally, 
     a z-table generator is also provided by p-value method.
+
+    References:
+    - Wikipedia contributors. (2020, December 19). Normal distribution. In Wikipedia, The Free Encyclopedia. Retrieved 10:44, 
+    December 22, 2020, from https://en.wikipedia.org/w/index.php?title=Normal_distribution&oldid=995237372
+    - Weisstein, Eric W. "Normal Distribution." From MathWorld--A Wolfram Web Resource. https://mathworld.wolfram.com/NormalDistribution.html
+
     Args: 
         mean(float): mean of the distribution
         std(float): standard deviation of the distribution
@@ -114,7 +120,7 @@ class Normal(Base):
         cdf - returns either plot of the distirbution or evaluation at randvar.
         p_value - returns p-value at randvar.
     '''
-    def __init__(self, mean, std, x):
+    def __init__(self, mean=0, std=1, x):
         self.mean = mean
         self.std = std
         self.randvar = x
@@ -129,9 +135,9 @@ class Normal(Base):
             ylabel=None):
         '''
         Args:
-            interval(int): defaults to none. Only necessary for defining scatter plot.
-            threshold(int): defaults to 100. Defines the sample points in scatter plot.
-            plot(bool): if true, returns scatter plot.
+            interval(int): defaults to none. Only necessary for defining plot.
+            threshold(int): defaults to 100. Defines the sample points in plot.
+            plot(bool): if true, returns plot.
             xlim(float): sets x axis ∈ [-xlim, xlim]. Only relevant when plot is true.
             ylim(float): sets y axis ∈[0,ylim]. Only relevant when plot is true. 
             xlabel(string): sets label in x axis. Only relevant when plot is true. 
@@ -161,9 +167,9 @@ class Normal(Base):
             ylabel=None):  # cdf ~ z-score?
         '''
         Args: 
-            interval(int): defaults to none. Only necessary for defining scatter plot.
-            threshold(int): defaults to 100. Defines the sample points in scatter plot.
-            plot(bool): if true, returns scatter plot.
+            interval(int): defaults to none. Only necessary for defining plot.
+            threshold(int): defaults to 100. Defines the sample points in plot.
+            plot(bool): if true, returns plot.
             xlim(float): sets x axis ∈ [-xlim, xlim]. Only relevant when plot is true.
             ylim(float): sets y axis ∈[0,ylim]. Only relevant when plot is true. 
             xlabel(string): sets label in x axis. Only relevant when plot is true. 
@@ -180,8 +186,9 @@ class Normal(Base):
             super().plot(x, y, xlim, ylim, xlabel, ylabel)
         return generator(randvar)
 
-    def p_value(self, x_lower=-(np.inf), x_upper=None):
+    def p_value(self, x_lower=-np.inf, x_upper=None):
         '''
+        Transforms distribution to standard normal, first. 
         Args:
             x_lower(float): defaults to infinity.
             x_upper(float): defaults to None. 
@@ -189,13 +196,44 @@ class Normal(Base):
         Returns:
             p-value drawn from normal distirbution between x_lower and x_upper.
         '''
+        mean = self.mean; std = self.std
+        if x_upper is None:
+            x_upper = self.randvar
+
+        x_upper = (x_upper - mean)/std
+        if x_lower!=-np.inf:
+            x_lower = (x_lower-mean)/std
         generator = lambda x: 1 / np.sqrt(2 * np.pi) * np.exp(-x**2 / 2)
         return sci.integrate.quad(generator, x_lower, x_upper)[0]
 
+    def confidence_interval(self):
+        # find critical values for a given p-value
+        pass
+
 
 class T_distribution(Base):
-    def __init__(self):
-        pass
+    '''
+    This class contains implementation of the Student's Distribution for calculating the
+    probablity density function and cumulative distirbution function. Additionally, 
+    a t-table generator is also provided by p-value method. Note that the implementation
+    of T(Student's) distribution is defined by beta-functions. 
+
+    References: 
+    - Kruschke JK (2015). Doing Bayesian Data Analysis (2nd ed.). Academic Press. ISBN 9780124058880. OCLC 959632184.
+    - Weisstein, Eric W. "Student's t-Distribution." From MathWorld--A Wolfram Web Resource. https://mathworld.wolfram.com/Studentst-Distribution.html
+
+    Args:
+        df(int): degrees of freedom. Defined as d.f. = n-1 where n is the sample size.
+        randvar(float): random variable. 
+    
+    Methods:
+        - pdf
+        - cdf
+        - p_value
+    '''
+    def __init__(self, df, randvar):
+        self.df = df
+        self.randvar = randvar
 
     def pdf(self,
             plot=False,
@@ -205,8 +243,29 @@ class T_distribution(Base):
             ylim=None,
             xlabel=None,
             ylabel=None):
+        '''
+        Args:
+            interval(int): defaults to none. Only necessary for defining plot.
+            threshold(int): defaults to 100. Defines the sample points in plot.
+            plot(bool): if true, returns plot.
+            xlim(float): sets x axis ∈ [-xlim, xlim]. Only relevant when plot is true.
+            ylim(float): sets y axis ∈[0,ylim]. Only relevant when plot is true. 
+            xlabel(string): sets label in x axis. Only relevant when plot is true. 
+            ylabel(string): sets label in y axis. Only relevant when plot is true. 
 
-        pass
+        
+        Returns: 
+            either probability density evaluation for randvar or plot of the T distribution.
+        '''
+        df = self.df; randvar = self.randvar
+        generator = lambda x, df: (1 / (np.sqrt(df) * ss.beta(
+            1 / 2, df / 2))) * np.power((1 + (x**2 / df)), -(df + 1) / 2)
+        if plot == True:
+            x = np.linspace(-interval,interval, int(threshold))
+            y = np.array([generator(i, df) for i in x])
+            super().plot(x, y, xlim, ylim, xlabel, ylabel)
+
+        return generator(randvar, df)
 
     def cdf(self,
             plot=False,
@@ -216,13 +275,71 @@ class T_distribution(Base):
             ylim=None,
             xlabel=None,
             ylabel=None):
+        '''
+        Args:
+            interval(int): defaults to none. Only necessary for defining plot.
+            threshold(int): defaults to 100. Defines the sample points in plot.
+            plot(bool): if true, returns plot.
+            xlim(float): sets x axis ∈ [-xlim, xlim]. Only relevant when plot is true.
+            ylim(float): sets y axis ∈[0,ylim]. Only relevant when plot is true. 
+            xlabel(string): sets label in x axis. Only relevant when plot is true. 
+            ylabel(string): sets label in y axis. Only relevant when plot is true. 
 
+        
+        Returns: 
+            either cumulative distirbution evaluation for some point or plot of the T distribution.
+        '''
+        df = self.df; randvar = self.randvar
+
+        def generator(x,df):
+            generator = lambda x, df: (1/(np.sqrt(df)*ss.beta(1/2,df/2)))*np.power((1+(x**2/df)),-(df+1)/2)
+            return sci.integrate.quad(generator,-np.inf, x, args=df)[0]
+
+        if plot == True:
+            x = np.linspace(-interval, interval, int(threshold))
+            y = np.array([generator(i, df) for i in x])
+            super().plot(x, y, xlim, ylim, xlabel, ylabel)
+
+        return generator(randvar, df)
+
+    def p_value(self, x_lower=-np.inf, x_upper = None):
+        '''
+        Args:
+            x_lower(float): defaults to -∞. Defines the lower value of the distribution. Optional.
+            x_upper(float): defaults to None. Defines the upper value of the distribution. Optional.
+
+            Note: definition of x_lower and x_upper are only relevant when probability is between two random variables.
+            Otherwise, the default random variable is x.
+            
+        Returns:
+            p-value of the Normal distribution evaluated at some random variable.
+        '''
+        df = self.df
+        if x_upper == None:
+            x_upper = self.randvar
+
+        generator = lambda x, df: (1 / (np.sqrt(df) * ss.beta(
+            1 / 2, df / 2))) * np.power((1 + (x**2 / df)), -(df + 1) / 2)
+
+        return sci.integrate.quad(generator, x_lower, x_upper, args=df)[0]
+
+    def confidence_interval(self): # for single means and multiple means
         pass
 
 
 class Cauchy(Base):
-    def __init__(self):
-        pass
+    '''
+    This class contains methods concerning the Cauchy Distribution. 
+    
+    References:
+    - Wikipedia contributors. (2020, November 29). Cauchy distribution. In Wikipedia, The Free Encyclopedia. 
+    Retrieved 12:01, December 22, 2020, from https://en.wikipedia.org/w/index.php?title=Cauchy_distribution&oldid=991234690
+    - Weisstein, Eric W. "Cauchy Distribution." From MathWorld--A Wolfram Web Resource. https://mathworld.wolfram.com/CauchyDistribution.html
+    '''
+    def __init__(self, x, location, scale):
+        self.scale = scale
+        self.location = location
+        self.x = x
 
     def pdf(self,
             plot=False,
@@ -232,8 +349,31 @@ class Cauchy(Base):
             ylim=None,
             xlabel=None,
             ylabel=None):
+        '''
+        Args:
+            interval(int): defaults to none. Only necessary for defining plot.
+            threshold(int): defaults to 100. Defines the sample points in plot.
+            plot(bool): if true, returns plot.
+            xlim(float): sets x axis ∈ [-xlim, xlim]. Only relevant when plot is true.
+            ylim(float): sets y axis ∈[0,ylim]. Only relevant when plot is true. 
+            xlabel(string): sets label in x axis. Only relevant when plot is true. 
+            ylabel(string): sets label in y axis. Only relevant when plot is true. 
 
-        pass
+        
+        Returns: 
+            either probability density evaluation for some point or plot of Cauchy distribution.
+        '''
+        x = self.x
+        location = self.location
+        scale = self.scale
+        generator = lambda x, location, scale: 1 / (np.pi * scale * (1 + (
+            (x - location) / scale)**2))
+        if plot == True:
+            x = np.linspace(-interval, interval, int(threshold))
+            y = np.array([generator(i, location, scale) for i in x])
+            super().plot(x, y, xlim, ylim, xlabel, ylabel)
+
+        return generator(x, location, scale)
 
     def cdf(self,
             plot=False,
@@ -243,7 +383,55 @@ class Cauchy(Base):
             ylim=None,
             xlabel=None,
             ylabel=None):
+        '''
+        Args:
+            interval(int): defaults to none. Only necessary for defining plot.
+            threshold(int): defaults to 100. Defines the sample points in plot.
+            plot(bool): if true, returns plot.
+            xlim(float): sets x axis ∈ [-xlim, xlim]. Only relevant when plot is true.
+            ylim(float): sets y axis ∈[0,ylim]. Only relevant when plot is true. 
+            xlabel(string): sets label in x axis. Only relevant when plot is true. 
+            ylabel(string): sets label in y axis. Only relevant when plot is true. 
 
+        
+        Returns: 
+            either cumulative distirbution evaluation for some point or plot of Cauchy distribution.
+        '''
+        x = self.x
+        location = self.location
+        scale = self.scale
+        generator = lambda x, location, scale: (1 / np.pi) * np.arctan(
+            (x - location) / scale) + 1 / 2
+        if plot == True:
+            x = np.linspace(-interval, interval, int(threshold))
+            y = np.array([generator(i, location, scale) for i in x])
+            super().plot(x, y, xlim, ylim, xlabel, ylabel)
+
+        return generator(x, location, scale)
+
+    def p_value(self, x_lower=None, x_upper=None):
+        '''
+        Args:
+            x_lower(float): defaults to None. Defines the lower value of the distribution. Optional.
+            x_upper(float): defaults to None. Defines the upper value of the distribution. Optional.
+
+            Note: definition of x_lower and x_upper are only relevant when probability is between two random variables.
+            Otherwise, the default random variable is x.
+
+        Returns:
+            p-value of the Cauchy distribution evaluated at some random variable.
+        '''
+        x = self.x; location = self.location, scale = self.scale
+        generator = lambda x, location, scale: (1/np.pi)*np.arctan((x-location)/scale)+1/2
+        if (x_lower and x_upper) !=None:
+            return generator(x_lower, location, scale)-generator(x_upper, location, scale)
+        if x_lower is not None:
+            x = x_lower
+        if x_upper is not None:
+            x = x_upper
+        return generator(x, location, scale)
+
+    def confidence_interval(self):
         pass
 
 
@@ -273,10 +461,14 @@ class F_distribution(Base):
 
         pass
 
+    def confidence_interval(self):
+        pass
+
 
 class Chi_distribution(Base):
-    def __init__(self):
-        pass
+    def __init__(self, df, x):
+        self.x = x
+        self.df = df
 
     def pdf(self,
             plot=False,
@@ -287,7 +479,14 @@ class Chi_distribution(Base):
             xlabel=None,
             ylabel=None):
 
-        pass
+        df = self.df; randvar = self.x
+        generator = lambda x,df = (1/(np.power(2,(df/2)-1)*ss.gamma(df/2)))*np.power(x,df-1)*np.exp(-x**2/2)
+        if plot == True: 
+            x = np.linspace(-interval, interval, int(threshold))
+            y = np.array([generator(i, df) for i in x])
+            super().plot(x, y, xlim, ylim, xlabel, ylabel)
+
+        return generator(randvar, df)
 
     def cdf(self,
             plot=False,
@@ -300,6 +499,8 @@ class Chi_distribution(Base):
 
         pass
 
+    def p_val(self):
+        pass 
 
 class Gamma_distribution(Base):
     def __init__(self):
