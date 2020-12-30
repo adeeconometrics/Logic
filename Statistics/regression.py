3ec0a026e1ceb8dfa0d11c56ceeab8061d94c4aa
Dave Amiana <amianism25@gmail.com>
2020-12-30 18:22:49 +0800

    first commit

--- Statistics/regression.py
+++ Statistics/regression.py
@@ -1,3 +1,92 @@
 # GLM
-class Leastsq:
+class generalMethods:
+    def __init__(self):
+        pass
+
+    def least_squares(self):
+        pass
+
+    def linear_least_sq(self):
+        pass
+
+    def non_linear_least_sq(self):
+        pass
+
+    def least_absolute_dev(self):
+        pass
+
+    def fit_curve(self):
+        pass
+
+    def f_test(self):
+        pass
+
+    def t_test(self):
+        pass
+
+    def cooefficient_determination(self):
+        pass
+
+    def multiple_correlation(self):
+        pass
+
+
+class Diagnostics:
+    pass
+
+
+class GLM(generalMethods):
+    def __init__(self):
+        pass
+
+    def logistic_ordered(self):
+        pass
+
+    def logistic_multinomial(self):
+        pass
+
+    def probit_ordered(self):
+        pass
+
+    def probit_multinomial(self):
+        pass
+
+    def poisson(self):
+        pass
+
+    def max_likelihood(self):
+        pass
+
+    pass
+
+
+class OLS(generalMethods):
+    pass
+
+
+class General_generalMethods(generalMethods):
+    pass
+
+
+class simpleLinearRegression(generalMethods):
+    pass
+
+
+class trendEstimation(generalMethods):
+    pass
+
+
+class ridgeRegression(generalMethods):
+    pass
+
+
+class polynomialRegression(generalMethods):
+    pass
+
+
+class segmentedRegression(generalMethods):
+    pass
+
+
+class nonLinearRegression(generalMethods):
     pass
