d064f9b4bb9a22a48ba72e7ac1bd502593fe35a0
Dave Amiana <amianism25@gmail.com>
2021-01-11 23:46:56 +0800

    cpp folders

---
+++ DataStructures/C++/Data.h
@@ -0,0 +1,14 @@
+# pragma once 
+
+class ListType{
+    private:
+        int list[1000];
+    public:
+        // methods
+        void addList();
+        void removeList();
+        bool isempt();
+        bool exists();
+        void clearList();
+        void searchList();
+}
